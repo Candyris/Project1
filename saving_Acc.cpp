7e14129a6eabeeb451d6dea458b1682646d965a2
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-03-04 00:59:33 +0530

    Update saving_Acc.cpp
    I have made adjustments to the program.
    
    if you like it than change to this.

--- saving_Acc.cpp
+++ saving_Acc.cpp
@@ -3,51 +3,94 @@
 using namespace std;
 
 class SavingsAccount {
-private:
-  double balance;
-  double interest_rate;
 
 public:
-  SavingsAccount(double balance, double interest_rate) {
-    this->balance = balance;
-    this->interest_rate = interest_rate;
+
+  // Initialing Class 
+  SavingsAccount(){}
+  SavingsAccount(double balance, double interest_rate) 
+                : m_balance(balance) ,m_interest_rate(interest_rate){}
+  
+  
+
+  // main Operation of class
+  void deposit(double amount);
+  void withdraw(double amount);
+  void calculateInterest();
+
+  // peeking for information Functions
+  void checkBalance();
+  double getBalance() { return m_balance; }
+  
+  
+  private:
+  double m_balance;
+  double m_interest_rate;
+
+};
+//Menu Handler
+  int selectChoice()
+  {
+    int choice;
+    cout << "Menu:" << endl;
+    cout << "1. Deposit" << endl;
+    cout << "2. Withdraw" << endl;
+    cout << "3. Calculate interest" << endl;
+    cout << "4. Check balance" << endl;
+    cout << "5. Exit" << endl;
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+    return choice;
   }
-
-  double getBalance() { return balance; }
-
-  void deposit(double amount) {
+//---------------------------------------------------Class defination---------------------------------------------------------------//  
+    void SavingsAccount::deposit(double amount) {
     if (amount <= 0) {
       cout << "Invalid deposit amount" << endl;
       return;
     }
 
-    balance += amount;
+    m_balance += amount;
     cout << "Deposit successful. You deposited Rupees " << amount << endl;
   }
 
-  void withdraw(double amount) {
+    void SavingsAccount::deposit(double amount)
+    {
+      if (amount <= 0) {
+      cout << "Invalid deposit amount" << endl;
+      return;
+    }
+
+    m_balance += amount;
+    cout << "Deposit successful. You deposited Rupees " << amount << endl;
+  }
+
+
+    void SavingsAccount::withdraw(double amount) {
     if (amount <= 0) {
       cout << "Invalid withdraw amount" << endl;
       return;
     }
 
-    if (amount > balance) {
+    if (amount > m_balance) {
       cout << "Insufficient balance" << endl;
       return;
     }
 
-    balance -= amount;
+    m_balance -= amount;
     cout << "Withdraw successful. You withdrew Rupees " << amount << endl;
   }
 
-  void calculateInterest() {
-    double interest = balance * interest_rate / 100;
-    balance += interest;
+  void SavingsAccount::calculateInterest() {
+    double interest = m_balance * m_interest_rate / 100;
+    m_balance += interest;
     cout << "Interest calculated successfully. You earned Rupees " << interest << endl;
   }
 
-  void checkBalance() { cout << "Your current balance is: Rupees " << balance << endl; }
-};
+  void SavingsAccount::checkBalance() { cout << "Your current balance is: Rupees " << m_balance << endl; }
+//---------------------------------------------------Class defination---------------------------------------------------------------//  
+
+
 
 int main() {
   double balance, interest_rate;
@@ -62,15 +105,8 @@ int main() {
   int choice;
   bool continue_loop = true;
   while (continue_loop) {
-    cout << "Menu:" << endl;
-    cout << "1. Deposit" << endl;
-    cout << "2. Withdraw" << endl;
-    cout << "3. Calculate interest" << endl;
-    cout << "4. Check balance" << endl;
-    cout << "5. Exit" << endl;
 
-    cout << "Enter your choice: ";
-    cin >> choice;
+  choice = selectChoice();
 
     switch (choice) {
       case 1:
